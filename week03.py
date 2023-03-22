424ba15dfd361a626649992f072d790f8a699d74
noooys <127947800+noooys@users.noreply.github.com>
2023-03-22 21:44:16 +0900

    Update week03.py

--- week03.py
+++ week03.py
@@ -1,11 +1,15 @@
-# week03_prime_number
-number = int(input("Input number : "))
-count = 0
+# week03_prime_number_quiz v0.8
+start_no = int(input("Enter starting number : "))
+end_no = int(input("Enter ending number : "))
+is_prime = True
 
-for i in range(1, number+1):
-    if number % i == 0:
-        count = count+1
-if count == 2:
-    print(f'{number} is prime number!')
-else:
-    print(f'{number} is NOT prime number.')
+for k in range(start_no, end_no+1):
+    if k < 2:
+        is_prime = False
+    else:
+        for i in range(2, k):
+            if k % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(k, end=' ')
