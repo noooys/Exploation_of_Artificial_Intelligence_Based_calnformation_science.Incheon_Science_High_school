bd92cd35a0d4922f997f3ba6e8a3223f647c0b52
noooys_ <syeoxonn@gmail.com>
2023-03-22 20:54:14 +0900

    Initial commit

---
+++ temperature_converter.py
@@ -0,0 +1,17 @@
+While true:
+    menu = int(input("1) farenheit to celcius  2) celcius to farenheit  3) exit : "))
+
+    if menu == "1":
+        farenheit = float(input('Enter temperature on Farenheit : '))
+		celsius = (farenheit -32.0) * (5.0/9.0)
+		print(f"{farenheit:.2f} degrees Farenheit is {celsius:.2f} degrees Cesius.")
+	elif menu == "2":
+		celsius = float(input("Enter teperature in Celsius : "))
+		farenheit = (celsius * 5.0/9/0) + 32.0
+		print(f"{celsius:.2f} degrees Celsius is {farenheit:.2f} degrees Farenheit.")
+	elif menu == "3":
+	    break
+	else:
+		print("Please choose from the given menu.")
+
+print("Program finished!")
